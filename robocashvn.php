2eacdd3728f1aa267206b4e29e74ed89f679dcf9
shencong <baothong.0705@gmail.com>
2023-05-15 16:40:18 +0700

    Update robocashvn.php

--- robocashvn.php
+++ robocashvn.php
@@ -1,6 +1,6 @@
 <?php
 
-$json = '{"table": "bleach", "condition": "Bleach Chapter 409"}';
+$json = "{'CASH':[{'NAME':'1','LOGO':'https://www.mbbank.com.vn/images/logo.png','AFFILIATELINK':'https://shorten.asia/Sf8vRmY6','DESCRIPTION':'DESCRIPTION','TITTLE1':'Vay nhanh không thế chấp', 'MSG1':'Số tiền vay từ 5-50 triệu đồng'},{'NAME':'2','LOGO':'congcong@gmail.com','AFFILIATELINK':'https://www.24h.com.vn/','DESCRIPTION':'DESCRIPTION'},{'NAME':'3','LOGO':'congcong@gmail.com','AFFILIATELINK':'https://vnexpress.net/','DESCRIPTION':'DESCRIPTION'},{'NAME':'4','LOGO':'congcong@gmail.com','AFFILIATELINK':'https://vnexpress.net/','DESCRIPTION':'DESCRIPTION'},{'NAME':'5','LOGO':'congcong@gmail.com','AFFILIATELINK':'https://vnexpress.net/','DESCRIPTION':'DESCRIPTION'}]}";
 
 echo $json;
 
